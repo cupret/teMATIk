6e60aa3be75b0e36de5d8ab1e21862c9071ea052
Starfall Production <starfallproduction42@gmail.com>
2019-09-02 14:39:47 +0700

    Implement fully tested navigation of Main(Promo), About(Company Information), and Detail

---
+++ app/src/main/java/id/ac/umn/tematik/Main.java
@@ -0,0 +1,78 @@
+package id.ac.umn.tematik;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.fragment.NavHostFragment;
+
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class Main extends Fragment {
+
+
+    public Main() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        // set toolbar
+        setHasOptionsMenu(true);
+        Toolbar toolbar = view.findViewById(R.id.toolbar);
+        ((MainActivity)getActivity()).setActionBar(toolbar);
+
+        final Fragment cur = this;
+        Button btn = view.findViewById(R.id.button);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View btn) {
+                MainDirections.ActionMainToDetail action = MainDirections.actionMainToDetail(10);
+                NavHostFragment.findNavController(cur).navigate(action);
+            }
+        });
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        inflater.inflate(R.menu.fragment_main_menu, menu);
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                getActivity().onBackPressed();
+                return true;
+            case R.id.main_menu_about:
+                NavHostFragment.findNavController(this).navigate(MainDirections.actionMainToAbout());
+                return true;
+                default:
+                    return super.onOptionsItemSelected(item);
+
+        }
+    }
+}
--- app/src/main/java/id/ac/umn/tematik/MainActivity.java
+++ app/src/main/java/id/ac/umn/tematik/MainActivity.java
@@ -1,7 +1,9 @@
 package id.ac.umn.tematik;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void setActionBar(Toolbar toolbar){
+        setSupportActionBar(toolbar);
+    }
+
+    public void setHomeButton(int icon){
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayShowHomeEnabled(true);
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeButtonEnabled(true);
+        actionBar.setHomeAsUpIndicator(icon);
     }
 }
