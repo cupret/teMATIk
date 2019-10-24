f030fc7b463894f881570ba24140ba0e7f2ffb92
Starfall Production <starfallproduction42@gmail.com>
2019-10-24 11:10:28 +0700

    Change DetailPromo view(UNTESTED)

--- app/src/main/java/id/ac/umn/tematik/DetailPromo.java
+++ app/src/main/java/id/ac/umn/tematik/DetailPromo.java
@@ -14,19 +14,20 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
-import android.util.Log;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.VideoView;
 
+import com.squareup.picasso.Picasso;
+import com.synnapps.carouselview.CarouselView;
+import com.synnapps.carouselview.ImageListener;
+
 import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -57,10 +58,24 @@ public class DetailPromo extends Fragment {
     };
 
     private TextView name;
-    private TextView date;
+    private TextView dateFrom;
+    private TextView dateTo;
     private TextView desc;
     private VideoView vid;
     private RecyclerView imgs;
+    private CarouselView promoImg;
+
+    private ArrayList<String> urls;
+    private ImageListener imageListener = new ImageListener() {
+        @Override
+        public void setImageForPosition(int position, ImageView imageView) {
+            String url = urls.get(position);
+            if(!url.isEmpty()) {
+                imageView.setScaleType(ImageView.ScaleType.FIT_CENTER);
+                Picasso.get().load(url).into(imageView);
+            }
+        }
+    };
 
     public DetailPromo() {
         // Required empty public constructor
@@ -87,14 +102,17 @@ public class DetailPromo extends Fragment {
         navController =  NavHostFragment.findNavController(this);
 
         // init views
-        name = view.findViewById(R.id.detail_promo_title);
-        date = view.findViewById(R.id.detail_promo_date);
+        name = view.findViewById(R.id.detail_product_title);
+        dateFrom = view.findViewById(R.id.detail_promo_date_from);
+        dateTo = view.findViewById(R.id.detail_promo_date_to);
         desc = view.findViewById(R.id.detail_promo_desc);
         vid = view.findViewById(R.id.detail_promo_vid);
         imgs = view.findViewById(R.id.detail_promo_imgs);
+        promoImg = view.findViewById(R.id.detail_promo_list_img);
+        promoImg.setImageListener(imageListener);
 
         // init recycle list
-        layoutManager = new GridLayoutManager(getContext(), 3);
+        layoutManager = new GridLayoutManager(getContext(), 4);
         productListAdapter = new ProductListAdapter(navController);
 
         // access promoId
@@ -104,8 +122,23 @@ public class DetailPromo extends Fragment {
             @Override
             public void onChanged(Promo promo) {
                 name.setText(promo.getName());
-                date.setText(promo.getStart_date() + " - " + promo.getEnd_date());
+                dateFrom.setText("From: " + promo.getStart_date());
+                dateTo.setText("To: " + promo.getEnd_date());
                 desc.setText(promo.getDescription());
+
+                // assign detail promo list img carousel view
+                urls = promo.getImages_url();
+                if(urls != null)
+                    promoImg.setPageCount(urls.size());
+                else
+                    promoImg.setPageCount(0);
+
+                // will display video if url exist
+                if(promo.getVideo_url().isEmpty()) {
+                    vid.setVisibility(View.GONE);
+                } else {
+                    vid.setVisibility(View.VISIBLE);
+                }
 
                 // update products livedata observer
                 for(LiveData<Product> product: productsLiveData.values()){
