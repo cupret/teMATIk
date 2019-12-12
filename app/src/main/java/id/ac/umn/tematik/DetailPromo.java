217dc3308eec6dbe2a00af52055a231d8824fddf
cupret <campret50@gmail.com>
2019-12-12 09:45:56 +0700

    tambah video di promo

--- app/src/main/java/id/ac/umn/tematik/DetailPromo.java
+++ app/src/main/java/id/ac/umn/tematik/DetailPromo.java
@@ -1,6 +1,11 @@
 package id.ac.umn.tematik;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
+import android.media.MediaPlayer;
+import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -15,15 +20,20 @@ import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.GridLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.os.Environment;
 import android.util.DisplayMetrics;
+import android.util.Log;
 import android.view.Display;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
 import android.view.WindowManager;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.MediaController;
 import android.widget.TextView;
 import android.widget.VideoView;
 
@@ -31,8 +41,15 @@ import com.squareup.picasso.Picasso;
 import com.synnapps.carouselview.CarouselView;
 import com.synnapps.carouselview.ImageListener;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -69,6 +86,7 @@ public class DetailPromo extends Fragment {
     private TextView dateFrom;
     private TextView dateTo;
     private TextView desc;
+    private LinearLayout cont_vid;
     private VideoView vid;
     private RecyclerView product_imgs;
     private CarouselView promoImg;
@@ -118,6 +136,7 @@ public class DetailPromo extends Fragment {
         dateFrom = view.findViewById(R.id.detail_promo_date_from);
         dateTo = view.findViewById(R.id.detail_promo_date_to);
         desc = view.findViewById(R.id.detail_promo_desc);
+        cont_vid = view.findViewById(R.id.detail_promo_cont_vid);
         vid = view.findViewById(R.id.detail_promo_vid);
         product_imgs = view.findViewById(R.id.detail_promo_imgs);
         promoImg = view.findViewById(R.id.detail_promo_list_img);
@@ -165,6 +184,23 @@ public class DetailPromo extends Fragment {
                     liveData.observe(owner, productObserver);
                     productsLiveData.put(code, liveData);
                 }
+
+                //init video
+                final MediaController media_controller = new MediaController(getContext());
+                vid.setMediaController(media_controller);
+                media_controller.setVisibility(View.GONE);
+
+                vid.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
+                    @Override
+                    public void onPrepared(MediaPlayer mp) {
+                        mp.setVolume(0,0);
+                        mp.setLooping(true);
+                    }
+                });
+                vid.setVisibility(View.INVISIBLE);
+                cont_vid.setVisibility(View.INVISIBLE);
+
+                new DownloadVideo(getContext()).execute(promoId+"", promo.getVideo_url());
             }
         });
 
@@ -190,4 +226,68 @@ public class DetailPromo extends Fragment {
 //                return super.onOptionsItemSelected(item);
 //        }
 //    }
+
+    private class DownloadVideo extends AsyncTask<String, Integer, String> {
+
+        private Context context;
+        private DownloadManager dm;
+
+        public DownloadVideo (Context context){ this.context = context; }
+
+        @Override
+        protected String doInBackground(String... args) {
+
+            if(args[1] != null) {
+                File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Video/promo_" + args[0]);
+                if(!file.exists()){
+                    Log.e("viddPromo", "!exist");
+                    try {
+                        URL url = new URL(args[1]);
+                        url.openConnection().connect();
+
+                        InputStream input = new BufferedInputStream(url.openStream());
+                        OutputStream output = new FileOutputStream(file);
+
+                        byte data[] = new byte[1024]; int count;
+                        while ((count = input.read(data)) != -1) output.write(data, 0, count);
+
+                        output.flush();
+                        output.close();
+                        input.close();
+
+                    } catch (Exception e) {
+                        Log.e("Error: ", e.getMessage());
+                    }
+                }
+                else{
+                    Log.e("viddPromo", "file already exist");
+                }
+
+                return "promo_" + args[0];
+            }
+            else {
+                Log.i("viddPromo", "no download");
+                return "nyeh";
+            }
+        }
+
+        @Override
+        protected void onPostExecute(String name) {
+            super.onPostExecute(name);
+
+            File file = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/Video/" + name);
+            if(file.exists()) {
+                vid.setVideoURI(Uri.parse(file.toString()));
+                vid.setVisibility(View.VISIBLE);
+                cont_vid.setVisibility(View.VISIBLE);
+                vid.start();
+                Log.i("viddPromo", "get video " + name);
+            } else {
+                vid.setVisibility(View.INVISIBLE);
+                Log.i("viddPromo", "no video");
+            }
+
+        }
+    }
+
 }
